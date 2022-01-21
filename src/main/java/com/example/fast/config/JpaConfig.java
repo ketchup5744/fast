4ec71f33a8574087d41d3599b515adcb5a89b68b
Josh <vkd5744@naver.com>
2022-01-21 16:20:16 +0900

    Created Config, Component

---
+++ src/main/java/com/example/fast/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.example.fast.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing // (JPA에 대한 감시 활성화)
+public class JpaConfig {
+}
--- src/test/java/com/example/fast/repository/UserRepositoryTest.java
+++ src/test/java/com/example/fast/repository/UserRepositoryTest.java
@@ -36,11 +36,11 @@ public class UserRepositoryTest extends FastApplicationTests {
         System.out.println("newUser : " + newUser);
          */
 
-        String account = "Test01";
-        String password = "Test01";
+        String account = "Test03";
+        String password = "Test03";
         String status = "REGISTERED";
         String email = "Test01@gmail.com";
-        String phoneNumber = "010-1111-2222";
+        String phoneNumber = "010-1111-3333";
         LocalDateTime registeredAt = LocalDateTime.now();
         LocalDateTime createdAt = LocalDateTime.now();
         String createdBy = "AdminServer";
@@ -52,8 +52,6 @@ public class UserRepositoryTest extends FastApplicationTests {
         user.setEmail(email);
         user.setPhoneNumber(phoneNumber);
         user.setRegisteredAt(registeredAt);
-        user.setCreatedAt(createdAt);
-        user.setCreatedBy(createdBy);
 
         User newUser = userRepository.save(user);
         Assert.assertNotNull(newUser);
