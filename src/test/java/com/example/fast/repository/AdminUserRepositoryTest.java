f899993e0e7add9ac4dfbf2bd22546d8c9844f9d
Josh <vkd5744@naver.com>
2022-01-21 15:08:21 +0900

    Created AdminUserRepositoryTest

---
+++ src/test/java/com/example/fast/repository/AdminUserRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.example.fast.repository;
+
+import com.example.fast.FastApplicationTests;
+import com.example.fast.model.entity.AdminUser;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class AdminUserRepositoryTest extends FastApplicationTests {
+
+    @Autowired
+    private AdminUserRepository adminUserRepository;
+
+    @Test
+    public void create() {
+        AdminUser adminUser = new AdminUser();
+
+        adminUser.setAccount("AdminUser01");
+        adminUser.setPassword("AdminUser01");
+        adminUser.setStatus("REGISTERED");
+        adminUser.setRole("PARTNER");
+        adminUser.setCreatedAt(LocalDateTime.now());
+        adminUser.setCreatedBy("AdminServer");
+
+        AdminUser newAdminUser = adminUserRepository.save(adminUser);
+        Assert.assertNotNull(newAdminUser);
+    }
+}
