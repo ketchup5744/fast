e192f33d56f06256183a400a0476aa336c99fa38
Josh <vkd5744@naver.com>
2022-01-25 13:41:51 +0900

    Created User API (DELETE

--- src/main/java/com/example/fast/service/UserApiLogicService.java
+++ src/main/java/com/example/fast/service/UserApiLogicService.java
@@ -101,7 +101,19 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
 
     @Override
     public Header delete(Long id) {
-        return null;
+
+        // 1. id -> repository -> user
+
+        Optional<User> optional = userRepository.findById(id);
+
+        // 2. repository -> delete
+
+        return optional.map(user -> {
+                    userRepository.delete(user);
+                    return Header.OK();
+                })
+                .orElseGet(() -> Header.ERROR("데이터없음"));         // 3. response return
+
     }
 
     private Header<UserApiResponse> response(User user) {
