3eb38f51ef9041424afc3aa751dc7132e4b8f770
Josh <vkd5744@naver.com>
2022-01-26 10:33:07 +0900

    Created OrderGroup API (CREATE)

---
+++ src/main/java/com/example/fast/controller/api/OrderGroupApiController.java
@@ -0,0 +1,41 @@
+package com.example.fast.controller.api;
+
+import com.example.fast.Ifs.CrudInterface;
+import com.example.fast.model.network.Header;
+import com.example.fast.model.network.request.OrderGroupApiRequest;
+import com.example.fast.model.network.response.OrderGroupApiResponse;
+import com.example.fast.service.OrderGroupApiLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/orderGroup")
+public class OrderGroupApiController implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+
+    @Autowired
+    private OrderGroupApiLogicService orderGroupApiLogicService;
+
+    @Override
+    @PostMapping("")
+    public Header<OrderGroupApiResponse> create(@RequestBody Header<OrderGroupApiRequest> request) {
+        return orderGroupApiLogicService.create(request);
+    }
+
+    @Override
+    @GetMapping("{id}")
+    public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
+        return null;
+    }
+
+    @Override
+    @PutMapping("")
+    public Header<OrderGroupApiResponse> update(@RequestBody Header<OrderGroupApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping("{id}")
+    public Header delete(@PathVariable Long id) {
+        return null;
+    }
+}
